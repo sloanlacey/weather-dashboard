18fb0e1550601dd712f7a2e6c38d5f44e28f08a8
sloanlacey <sloanlacey89@gmail.com>
2020-12-31 17:27:31 -0800

    Add JS functions for display of 5-day forecast, add UV index info for color changes based on severity, update css, update bootstrap framework after materialize issues.

--- Develop/index.js
+++ Develop/index.js
@@ -1,44 +1,134 @@
 var newDate = new Date();
-  var nowDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-  var displayDate = document.getElementById('currentDay');
+var nowDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+var displayDate = document.getElementById('currentDay');
   displayDate.innerHTML = nowDate;
 //   Variables for time
-  var m = moment();
-  var nowTime = m.format('LTS');
-  var nowHour = newDate.getHours();
+var m = moment();
+var nowTime = m.format('LTS');
+var nowHour = newDate.getHours();
 
-
-// API Key for OpenWeather
 const apiKey = 'b7b1f0ad70509ca23301b5728eb11e20';
 
-// Search button event listener
-$( '#search-btn' ).click(searchButton);
-// Function for city input field and search button
-function searchButton() {
-    let citySearch = $('#search-field');
-    let cityChoice = citySearch.val();
-    citySearch.val('');
-    cityWeatherInfo(cityChoice);
+$(document).ready(init);
+// Event listener for clicking search button
+function init () {
+    $('nav button').click(searchButton);
+    getCityButtons();
 }
-// AJAX call
-function cityWeatherInfo(cityChoice) {
+
+function searchButton() {
+//grab search bar input
+var inputCity = $('nav input');
+var cityChoice = inputCity.val();
+inputCity.val('');
+//then call getCityData
+cityData(cityChoice); 
+}
+// First ajax call
+function cityData(cityChoice) {
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityChoice}&appid=${apiKey}&units=imperial`;
-    $.ajax ({
+    $.ajax({
         url: queryURL,
         method: 'GET'
-    }).then(displayWeather)
+    }).then(cityLatLon);
 }
-// Function to display weather data
-function displayWeather(data) {
+// Get location by latitude and longitude
+function cityLatLon(data) {
+    mainForecastDisplay(data);
+    var cityChoice = data.name;
+    addCityButton(cityChoice);
+    getCityButtons();
+    var lat = data.coord.lat;
+    var long = data.coord.lon;
+    getLatLonForecast(lat,long);
+}
+// Second ajax call
+function getLatLonForecast(lat,long){
+    var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`;
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(diaplayFiveDay);
+}
+// UV Color display by severity
+function uvIndexDisplay (UV) {
+    var uvIndex = 'favorable';
+    if(UV > 6) {
+        uvIndex = 'severe';  
+    } else if (UV>=3) {
+        uvIndex = 'moderate';
+    }
+    $('#UV').addClass(uvIndex).text('UV Index: ' + UV);
+}
+// 5-day Tiles Display
+function diaplayFiveDay (data) {
+    console.log(data);
+    var UV = data.daily[0].uvi;
+    uvIndexDisplay(UV); 
+    var html = '<h3>5 Day Forecast:</h3>';
+    for (let i = 1; i < 6; i++) {
+        var day = data.daily[i];
+        var temp = day.temp.day;
+        var humidity = day.humidity;
+        var icon = day.weather[0].icon;
+        var date = (new Date(day.dt*1000)).toDateString();
+        html += `
+        <div class="col-md-2 boxes">
+            <h4>${date}</h4>
+            <img src='http://openweathermap.org/img/wn/${icon}@2x.png'>
+            <p>Temperature: ${temp}&deg;F</p>
+            <p>Humidity: ${humidity}%</p>   
+        </div>
+        `;
+    }
+    $('#row-3').html(html);
+}
+// Main forecast display for current day (includes UV index)
+function mainForecastDisplay(data) {
     var temp = data.main.temp;
-    var city = data.name;
+    var cityChoice = data.name;
     var humid = data.main.humidity;
     var wind = data.wind.speed;
-    var displayInfo = `
-            <h2>${city}</h2>
-            <p>Temp: ${temp}&deg;F</p>
-            <p>Humidity: ${humid}%</p>
-            <p>Wind Speed: ${wind}mph</p>
-        `;
-    $(".two").html(displayInfo);
+    var date = (new Date(data.dt*1000)).toDateString();
+    var icon = data.weather[0].icon;
+    var html = `
+        <h2>${cityChoice} ${date}</h2> 
+        <img src='http://openweathermap.org/img/wn/${icon}@2x.png'>
+        <p>Temperature: ${temp}&deg;F</p>
+        <p>Humidity: ${humid}%</p>
+        <p>Wind Speed: ${wind} mph</p>
+        <p id='UV'></p>
+    `;
+    $("#main .card-body").html(html);
+ }
+
+function getStoredCities () {
+    var cities = localStorage.getItem('cities');
+    if(cities){
+        return JSON.parse(cities);
+    } 
+    return [];
+}
+
+function addCityButton(cityChoice) {
+    var cities = getStoredCities();
+    if(!cities.includes(cityChoice)) {
+        cities.unshift(cityChoice);
+        localStorage.setItem('cities', JSON.stringify(cities));
+    }
+}
+
+function getCityButtons () {
+    var cities = getStoredCities();
+    var html='';
+    for(let cityChoice of cities){
+        html+=`<button type='button' class='list-group-item list-group-item-action'>${cityChoice}</button>`;
+    }
+    $('#city-buttons').html(html);
+    $('#city-buttons button').click(clickSavedCityButtons)
+}
+
+function clickSavedCityButtons () {
+    var cityChoice = $(this).text();
+    cityData(cityChoice);
 }
